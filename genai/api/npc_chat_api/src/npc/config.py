c0b8ab57c01c851277ad9f7d92df06e6351ca5df
Ivy Gooch <igooch@google.com>
2024-03-12 20:01:29 +0000

    Changes from mounting the NPC config files with Docker to mounting via ConfigMap

--- genai/api/npc_chat_api/src/npc/config.py
+++ genai/api/npc_chat_api/src/npc/config.py
@@ -14,8 +14,8 @@
 
 import tomllib
 
-CONFIG_PATH="config/config.toml"
-WORLD_PATH="config/world.toml"
+CONFIG_PATH="/config/config.toml"
+WORLD_PATH="/config/world.toml"
 
 def data_from_file(path):
     with open(path, "rb") as f:
